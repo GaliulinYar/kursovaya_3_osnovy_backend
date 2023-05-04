a5b166ac73f5f4960bdc2d54e0c1d61645ba6e4a
Yaroslav Galiulin <yaroslav66@list.ru>
2023-05-04 17:14:47 +0500

    Загрузка дополнительных пакетоа

--- tests/test_get_list_func.py
+++ tests/test_get_list_func.py
@@ -7,4 +7,3 @@ def test_get_list_func():
     assert get_list_func.get_list_func('/home/yaroslav/kursovaya_3_osnovy_backend/tests/test_file.json') == [{"state": "EXECUTED", "operation": "op1"}, {"state": "EXECUTED", "operation": "op2"}]
     assert get_list_func.get_list_func('/home/yaroslav/kursovaya_3_osnovy_backend/tests/test_file2.json') == []
     assert get_list_func.get_list_func('/home/yaroslav/kursovaya_3_osnovy_backend/tests/test_file2.json') == []
-
